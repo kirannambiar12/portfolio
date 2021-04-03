6c2196aa24de128aba9db90025d4670d65e27400
kiran-nambiar <kiran.un@1stmain.co>
2021-04-03 23:39:54 +0530

    fixed es lint

--- src/components/Widgets/NewsApi/NewsCards.jsx
+++ src/components/Widgets/NewsApi/NewsCards.jsx
@@ -4,11 +4,11 @@ const NewsCards = ({ article }) => {
     return (
         <div className="card news-card background-black">
             <div>
-            <img className="img-fluid card-img-top" src={article?.urlToImage || "https://media.ticketmaster.co.uk/tm/en-gb/img/mw/images/f0/87/122346p1.jpg"} alt={article?.title} />
+            <img className="img-fluid card-img-top" src={article?.urlToImage || "https://media.ticketmaster.co.uk/tm/en-gb/img/mw/images/f0/87/122346p1.jpg"} alt={article?.title} alt={article?.title} />
             </div>
             <div className="card-body color-white">
                 <h6 className="news-card-title">{article?.title}</h6>
-                <a href={article?.url} target="_blank" className="btn read-more-news mt-3">Read More</a>
+                <a href={article?.url} target="_blank" rel="noopener noreferrer" className="btn read-more-news mt-3">Read More</a>
             </div>
         </div>
     )
